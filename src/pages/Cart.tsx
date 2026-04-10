4db7211e531b6ce4166dfc6cb4819e0dbf440032
GertMatthias <eljas@tlu.ee>
2026-04-10 13:45:49 +0300

    cart, homepage muudatused

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 
 function Cart() {
+const [orderRow, setOrderRows] = useState(JSON.parse(localStorage.getItem("cart") || "[]"));
+
   return (
-    <div>Cart</div>
+    <div>
+      {orderRow.map(orderRow =>
+        <div key={orderRow.product.id}>
+          <div>{orderRow.product.name}</div>
+          <div>{orderRow.product.price}$</div>
+          <div>{orderRow.quantity}tk</div>
+          <div>{orderRow.product.price * orderRow.quantity}$</div>
+          <button>x</button>
+        </div>
+      )}
+    </div>
   )
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -51,6 +51,18 @@ function HomePage() {
     setPage(0);
   }
 
+  const addToCart = (product: Product) => {
+    const cart = JSON.parse(localStorage.getItem("cart") || "[]");
+    const foundProduct = cart.find(cartProduct => cartProduct.product.id === product.id);
+    if (foundProduct) {
+      foundProduct.quantity++;
+    } else {
+      cart.push({product: product, quantity: 1});
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+  }
+
   // tõlge: https://react.i18next.com/guides/quick-start
   return (
     <div>
@@ -93,6 +105,7 @@ function HomePage() {
       {products.map(product => 
         <div key={product.id}>
           {product.name} - {product.price}€
+          <button onClick={() => addToCart(product)}>Lisa ostukorvi</button>
         </div>)}
 
       <button disabled={page === 0} onClick={() => setPage(page - 1)}>Eelmine</button>
